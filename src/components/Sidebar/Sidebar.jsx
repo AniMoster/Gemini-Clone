5485f098e585c77701bafd7a6fb7e0c656f47f62
Tejas <tejat630@gmail.com>
2024-12-08 14:49:05 +0530

    Sidebar Transition

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,41 +5,45 @@ import { assets } from "../../assets/assets";
 const Sidebar = () => {
   const [extended, setExtended] = useState(false);
 
+  const TypingText = ({ text }) => {
+    return <span className={extended ? "typing" : "removing"}>{text}</span>;
+  };
+
   return (
-    <div className="sidebar">
+    <div className={`sidebar ${extended ? "extended" : ""}`}>
       <div className="top">
         <img
           onClick={() => setExtended((prev) => !prev)}
           className="menu"
           src={assets.menu_icon}
-          alt=""
+          alt="Menu"
         />
         <div className="new-chat">
-          <img src={assets.plus_icon} alt="" />
-          {extended ? <p>New Chat</p> : null}
+          <img src={assets.plus_icon} alt="New Chat" />
+          {extended && <p>New Chat</p>}
         </div>
-        {extended ? (
+        {extended && (
           <div className="recent">
             <p className="recent-title">Recent</p>
             <div className="recent-entry">
-              <img src={assets.message_icon} alt="" />
+              <img src={assets.message_icon} alt="Message" />
               <p>What is React ...</p>
             </div>
           </div>
-        ) : null}
+        )}
       </div>
       <div className="bottom">
         <div className="bottom-item recent-entry">
-          <img src={assets.question_icon} alt="" />
-          {extended ? <p>Help</p> : null}
+          <img src={assets.question_icon} alt="Help" />
+          {extended && <p>Help</p>}
         </div>
         <div className="bottom-item recent-entry">
-          <img src={assets.history_icon} alt="" />
-          {extended ? <p>Activity</p> : null}
+          <img src={assets.history_icon} alt="Activity" />
+          {extended && <p>Activity</p>}
         </div>
         <div className="bottom-item recent-entry">
-          <img src={assets.setting_icon} alt="" />
-          {extended ? <p>Settings</p> : null}
+          <img src={assets.setting_icon} alt="Settings" />
+          {extended && <p>Settings</p>}
         </div>
       </div>
     </div>
